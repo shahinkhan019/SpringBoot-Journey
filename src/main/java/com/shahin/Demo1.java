5a350d56eab897ad6f74bea6538c6ff2319fead8
shahinkhan019 <akhan366115@gmail.com>
2025-12-10 17:20:59 +0530

    to avoid confusions i have passed method name directly instead of class

---
+++ src/main/java/com/shahin/Demo1.java
@@ -0,0 +1,17 @@
+package com.shahin;
+
+import com.shahin.repository.OwnerRepository;
+import com.shahin.repository.imp1.OwnerRepositoryImp1;
+import com.shahin.service.OwnerService;
+import com.shahin.service.imp1.OwnerServiceImp1;
+
+public class Demo {
+    public static void main(String[] args) {
+//        OwnerRepository ownerRepository = new OwnerRepositoryImp1();
+        ApplicationContext context  = new AnnotationConfigApplicationContext(SpringConfig.class); // i am using this context bcz it can read the config class which is annotated with @Configuration
+//        OwnerService ownerService = context.getBean(OwnerService.class); it is getting confused which bean to create bcz we have two service method with same type then now just pass the method name as bean
+//        OwnerService ownerService = (OwnerService) context.getBean("ownerService2"); instead of type casting i can do this like below also
+        OwnerService ownerService = context.getBean("ownerServiceReal", OwnerService.class);// now it will create the bean of type OwnerServiceImp1\
+        System.out.println(ownerService.findOwner());
+    }
+}
