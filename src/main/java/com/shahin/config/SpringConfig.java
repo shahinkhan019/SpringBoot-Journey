e1c329cbfce400e48f0ba086d9adbe0b0b01d1b6
shahinkhan019 <akhan366115@gmail.com>
2025-12-12 19:06:19 +0530

    using @Configuration

--- src/main/java/com/shahin/config/SpringConfig.java
+++ src/main/java/com/shahin/config/SpringConfig.java
@@ -6,6 +6,10 @@ import com.shahin.service.mock.imp1.OwnerServiceImp1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+// ({ "com.shahin.repository.imp1", "com.shahin.service.imp1" }) in enterprise level application there can be  a lots of
+// packages so its will be difficult for me to write complete name of that particular package, now we can just write the main package name and spring will scan all the sub packages automatically
+@ComponentScan({ "com.shahin" })  // what it does is it will scan the specified packages for classes annotated with @Component and register them as beans in the Spring application context.
 @Configuration
 public class SpringConfig { // i am using this class to configure my beans but why? bcz in real world application we may have multiple implementation
     // of same interface so to avoid confusion we can use this config class to define which implementation to use
